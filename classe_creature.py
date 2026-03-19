9eb9b033ea31132a2b0fd6090404fdc509bbda3e
sbakoren <sbakoren@gmail.com>
2026-03-19 16:36:28 +0100

    Creation de la classe creature

---
+++ classe_creature.py
@@ -0,0 +1,11 @@
+class Creature:
+
+    def __init__(self, nom, description, pv, defense, type_degats):
+        self.nom = nom
+        self.description = description
+        self.pv = pv
+        self.defense = defense
+        self.type_degats = type_degats
+        self.actions = []
+        self.initiative = 0
+        self.resistances = []
