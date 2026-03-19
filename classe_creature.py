8f0b140b1fa4c4eb2673ba528f0c3a698f719c89
sbakoren <sbakoren@gmail.com>
2026-03-19 16:44:36 +0100

    Création de la classe hero

--- classe_creature.py
+++ classe_creature.py
@@ -9,3 +9,17 @@ class Creature:
         self.actions = []
         self.initiative = 0
         self.resistances = []
+
+
+class Hero(Creature):
+
+    def __init__(self, nom, description, pv, defense, arme):
+        self.nom = nom
+        self.description = description
+        self.pv = pv
+        self.defense = defense
+        self.type_degats = arme.type_degats
+        self.actions = []
+        self.initiative = 0
+        self.resistances = []
+        self.arme = arme
