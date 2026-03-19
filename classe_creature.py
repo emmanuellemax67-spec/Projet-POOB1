e6ebbc028b7100c5e9252246b11dd45627ecb146
sbakoren <sbakoren@gmail.com>
2026-03-19 16:49:04 +0100

    Création de la fonction d'affichage de caractéristiques

--- classe_creature.py
+++ classe_creature.py
@@ -23,3 +23,12 @@ class Hero(Creature):
         self.initiative = 0
         self.resistances = []
         self.arme = arme
+
+    def afficher_caracteristiques(self):
+        print("\n--- HÉROS ---")
+        print("Nom :", self.nom)
+        print("Description :", self.description)
+        print("PV :", self.pv)
+        print("Défense :", self.defense)
+        print("Type dégâts :", self.type_degats)
+        print("Arme :", self.arme.nom)
