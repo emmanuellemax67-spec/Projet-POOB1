067968bfb09913e4be340d9226516b9e43baf148
Ingrid <tchakounteingrid237@gmail.com>
2026-03-19 17:01:29 +0100

    Création du dictionnaire météo

---
+++ fonctionnalites.py
@@ -0,0 +1,9 @@
+import random
+
+
+METEOS = {
+    "Soleil":  {"Feu": 2, "Glace": -2},
+    "Pluie":   {"Feu": -2, "Perçant": 1},
+    "Orage":   {"Magique": 2, "Feu": -1},
+    "Neige":   {"Tranchant": 1, "Feu": -3},
+}
